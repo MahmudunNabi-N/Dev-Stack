6c34869f2c5df6ef431bea9e388b9a028ffe97a3
MahmudunNabi-N <mnnahid25@proton.me>
2026-09-24 23:57:52 +0600

    deploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import Banner from "./components/Banner"
-import Footer from "./components/footer"
+import Footer from "./components/Footer"
 import Nav from "./components/Nav"
 import TechnologySection from "./components/TechnologySection"
 
